42f5c631200eb2bba0a8e29511894966a9d4f8cb
Eas <me@eas.web.id>
2022-09-19 04:34:43 +0700

    update: Add query based from username

--- src/main/java/net/interview/springauth/user/UserRepository.java
+++ src/main/java/net/interview/springauth/user/UserRepository.java
@@ -1,8 +1,14 @@
 package net.interview.springauth.user;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<Users, Long> {
+    @Query("SELECT u FROM Users u WHERE u.username = :username")
+    Optional<Users> findOneByUsername(@Param("username") String username);
 }
