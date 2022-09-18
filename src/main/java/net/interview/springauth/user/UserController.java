dfc5e39f77baf6312745c04ead8d4eeb58acd643
Eas <me@eas.web.id>
2022-09-19 04:35:19 +0700

    add: new login feature, JWT response update: registration process

--- src/main/java/net/interview/springauth/user/UserController.java
+++ src/main/java/net/interview/springauth/user/UserController.java
@@ -1,13 +1,18 @@
 package net.interview.springauth.user;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import net.interview.springauth.helper.HashHelper;
+import net.interview.springauth.user.dto.UserLoginDto;
+import net.interview.springauth.user.dto.UserRegistrationDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -22,8 +27,39 @@ public class UserController {
           consumes = MediaType.APPLICATION_JSON_VALUE,
           produces = MediaType.APPLICATION_JSON_VALUE
   )
-  public ResponseEntity<?> login(@RequestBody() User user) {
-    return new ResponseEntity(HttpStatus.OK);
+  public ResponseEntity<?> login(@Valid @RequestBody UserLoginDto user, BindingResult result) {
+    HashHelper hh = new HashHelper();
+    String jwToken;
+
+    try {
+      if(result.hasErrors()){
+        return new ResponseEntity<>(result.getAllErrors(), HttpStatus.BAD_REQUEST);
+      }
+
+      Users _user = userRepository.findOneByUsername(user.getUsername()).orElseThrow(
+              () -> new UsernameNotFoundException("Sorry, username is not found.")
+      );
+
+      if(hh.checkPassword(user.getPassword().toCharArray(), _user.getPassword())) {
+        jwToken = Jwts.builder()
+                .setIssuer("SpringTest")
+                .setSubject("UsernameTokenIdentifier")
+                .claim("username", _user.getUsername())
+                .signWith(
+                        SignatureAlgorithm.HS256,
+                        "SuperSecretThingyThatShouldWorkAfterGeneratingAFewMoreWordsIntoItAndItShouldWorkThisTime."
+                )
+                .compact();
+      } else {
+        // this will ensure that the stored is secure, even if it's found.
+        throw new UsernameNotFoundException("Sorry, username is not found.");
+      }
+
+      return new ResponseEntity<String>(jwToken, HttpStatus.OK);
+    } catch (Exception e) {
+      System.out.println(e.toString());
+      return new ResponseEntity<>(e.getLocalizedMessage(), HttpStatus.BAD_REQUEST);
+    }
   }
 
   @PostMapping(
@@ -31,13 +67,13 @@ public class UserController {
           consumes = MediaType.APPLICATION_JSON_VALUE,
           produces = MediaType.APPLICATION_JSON_VALUE
   )
-  public ResponseEntity<?> register(@Valid @RequestBody User user, BindingResult result) {
+  public ResponseEntity<?> register(@Valid @RequestBody UserRegistrationDto user, BindingResult result) {
     try {
       if(result.hasErrors()){
         return new ResponseEntity<>(result.getAllErrors(), HttpStatus.BAD_REQUEST);
       }
 
-      userRepository.save(user);
+      userRepository.save(user.toUser());
 
       return new ResponseEntity<>(HttpStatus.CREATED);
     } catch (Exception e) {
