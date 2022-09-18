726787f449fa173ab9f41c73701c7cc70e41b041
Eas <me@eas.web.id>
2022-09-19 04:23:45 +0700

    update: rename model to follow table names add: @Data annotation

--- src/main/java/net/interview/springauth/user/Users.java
+++ src/main/java/net/interview/springauth/user/Users.java
@@ -1,5 +1,9 @@
 package net.interview.springauth.user;
 
+import lombok.Data;
+import lombok.experimental.Accessors;
+import net.interview.springauth.helper.HashHelper;
+
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 import javax.persistence.*;
@@ -7,29 +11,27 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
 import java.util.Base64;
 
 @Entity
 @Table(
-        name = "Users",
+        name = "users",
         uniqueConstraints = {
                 @UniqueConstraint(columnNames = "username"),
                 @UniqueConstraint(columnNames = "mobile_phone")
         }
 )
-public class User {
-    private SecureRandom rand = new SecureRandom();
-    private byte[] salt = new byte[14];
-
+@Data
+@Accessors(chain = true)
+public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
     @NotBlank(message = "Username cannot be empty!")
-    @Column(name = "username")
+    @Column(name = "username", unique = true)
     private String username;
 
     @NotBlank(message = "Mobile phone cannot be empty!")
@@ -49,9 +51,9 @@ public class User {
     @Column(name = "password")
     private String password;
 
-    public User () throws NoSuchAlgorithmException {}
+    public Users() throws NoSuchAlgorithmException {}
 
-    public User (
+    public Users(
             @NotBlank(message = "Username cannot be empty!")
             String username,
             @NotBlank(message = "Mobile phone cannot be empty!")
@@ -71,42 +73,5 @@ public class User {
         this.username = username;
         this.mobile_phone = mobile_phone;
         this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getMobile_phone() {
-        return mobile_phone;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setMobile_phone(String mobile_phone) {
-        this.mobile_phone = mobile_phone;
-    }
-
-    public void setPassword(String password) throws InvalidKeySpecException, NoSuchAlgorithmException {
-        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 128);
-        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-        byte[] hash = factory.generateSecret(spec).getEncoded();
-        String encodedHash = Base64.getEncoder().encodeToString(hash);
-
-        this.password = encodedHash;
-    }
-
-    public void setId(long id) {
-        this.id = id;
     }
 }
